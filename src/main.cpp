552e2196e58691c9da4ef2265b7bb9c96d441018
kotobazza <152398719+kotobazza@users.noreply.github.com>
2024-10-12 14:09:09 +0300

    Default structure

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <array>       // for array
+#include <chrono>      // for milliseconds
+#include <functional>  // for function
+#include <memory>      // for __shared_ptr_access, shared_ptr, allocator
+#include <string>      // for string, char_traits, operator+, basic_string
+#include <vector>      // for vector
+#include <archive.h>
+#include <archive_entry.h>
+//#include <stdio.h>
+#include <filesystem>
+#include <sys/statvfs.h>
+#include <mntent.h>
+#include <cstdio>
+#include <cstring>
+
+#include <fstream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+
+
+int main(){
+    std::cout << "Hello World!";
+}
