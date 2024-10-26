3ad1a2f351af3c85a8c5d11c6f2abcad002aa74c
kotobazza <152398719+kotobazza@users.noreply.github.com>
2024-10-26 21:18:23 +0300

    v0.0.6.3 Includes updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,23 @@
-
-
-#include <iostream>
-// #include <array>       // for array
-// #include <chrono>      // for milliseconds
-// #include <functional>  // for function
-// #include <memory>      // for __shared_ptr_access, shared_ptr, allocator
-// #include <string>      // for string, char_traits, operator+, basic_string
-// #include <vector>      // for vector
-
-
-#include "DiskInfo.h"
-#include "FileManipulations.h"
-#include "Headers/ArchiveWorker.h"
-#include "Headers/FileManipulations.h"
-#include "Headers/JsonWorker.h"
-#include "Headers/Serializable.h"
-#include "Headers/XmlWorker.h"
-#include "TextWorker.h"
-#include "JsonWorker.h"
-#include "XmlWorker.h"
-#include "ArchiveWorker.h"
-#include "Serializable.h"
-
 #include "Application.h"
-auto screen = ftxui::ScreenInteractive::Fullscreen();
+#include <iostream>
 
+
+
+
+auto screen = ftxui::ScreenInteractive::Fullscreen();
 
 void exitor()
 {
     screen.Exit();
 }
 
-int main(){
+
+int main()
+{
     using namespace std::string_literals;
-    std::cout << "Hello World!"<<std::endl;
-    
+    std::cout << "Hello World!" << std::endl;
+
     Application a(exitor);
-    
+
     screen.Loop(a.render());
-    
-    
-
-
-    
 }
